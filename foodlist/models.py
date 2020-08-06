961660526be96a2ea7a337fddd6b4daf4f61362e
Jacopo Kahl <jacopokahl@gmail.com>
2020-08-06 23:52:57 +0200

    Setup App models

--- foodlist/models.py
+++ foodlist/models.py
@@ -1,3 +1,15 @@
+from __future__ import unicode_literals #Database can read all the languages
 from django.db import models
 
 # Create your models here.
+
+class FoodList(models.Model):
+
+    productName = models.TextField()
+    productCategory = models.TextField()
+    productBuyDate = models.DateField()
+    productExpDate = models.DateField()
+
+    def __str__(self):
+        return self.productName + self.productCategory
+
