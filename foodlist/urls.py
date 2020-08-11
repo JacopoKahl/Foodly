7f11b28a447bd178505ac60f160b5e380e720daa
Jacopo Kahl <jacopokahl@gmail.com>
2020-08-11 21:50:01 +0200

    Error in model FoodRecipes

--- foodlist/urls.py
+++ foodlist/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name = 'home'),
     url(r'^features/$', views.features, name = 'features'),
+    url(r'^recipes/$', views.recipes, name = 'recipes'),
 ]
