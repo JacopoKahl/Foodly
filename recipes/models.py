01dac6ad3fcaa6be782786728905fd9613619cf7
Jacopo Kahl <jacopokahl@gmail.com>
2020-08-12 12:26:01 +0200

    Solved model recipes error

--- recipes/models.py
+++ recipes/models.py
@@ -1,27 +1,28 @@
 from __future__ import unicode_literals #Database can read all the languages
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from datetime import datetime
+from django.utils import timezone
 
-STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
-)
 
 class FoodRecipes(models.Model):
 
-    recipeName = models.CharField(u'Recipe name', help_text=u'The name of the recipe', max_length=30)
-    '''
-    recipeSlug = models.SlugField(u'Recipe slug', help_text=u'The slug URL', max_length=200, default='your-url')
-    #author = models.ForeignKey(u'Author', User, on_delete= models.CASCADE,related_name='blog_recipes', default='admin')
-    recipeContent = models.TextField(u'Recipe content', help_text=u'Write here your recipe', default='Write here..')
-    updated_on = models.DateField(u'Updated On')
-    created_on = models.DateField(u'Created')
-    recipeStatus = models.IntegerField(choices=STATUS, default=0)
+    STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    )
 
+    recipeName = models.CharField(u'Recipe name', help_text=u'The name of the recipe', max_length=30)
+    slug = models.SlugField(max_length = 250, unique_for_date='publish', default='-')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='User', null=True)
+    recipeContent = models.TextField(u'Recipe content', help_text=u'Write here your recipe', default='-')    
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     class Meta:
-        ordering = ['-created_on']
-    '''
+        ordering = ('-publish',)
+
     def __str__(self):
 
         return self.recipeName
